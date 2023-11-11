5d916ff7603226500b01fa61dd1900ca74e284f1
JAZZC0D3 <jazulescalzo@gmail.com>
2023-11-11 15:55:30 -0300

    Entrega TP 3

---
+++ myapp/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import ItemDetail from './components/itemDetail/ItemDetail'
+function App() {
+ 
+  return (
+    <>
+     <ItemDetail title="Nombre" description="Descripcion de mi pedido a modo de ejemplo para poder resolver mi TP3" price="$850" sku="00AD85CF37/1" STOCK="17" ></ItemDetail>
+    </>
+  )
+}
+
+export default App
